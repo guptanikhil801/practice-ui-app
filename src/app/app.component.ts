3e5660b59528d40d81946ca0a9aef0f48cec5381
Nikhil Gupta <guptanikhil801@gmail.com>
2026-05-20 22:19:02 +0530

    naming conventions changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RxjsOperatorsService } from './services/rxjs-operators.service';
-import { forkJoin, map, Subject } from 'rxjs';
+import { concatMap, delay, distinct, distinctUntilChanged, forkJoin, map, mergeMap, of, Subject, switchMap } from 'rxjs';
 import { UserComponent } from "./user/user.component";
 import { HttpClient } from '@angular/common/http';
 import { order } from './models/order.model';
@@ -10,45 +10,42 @@ import { error } from 'node:console';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserComponent],
+  imports: [UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'practice-ui-app';
   private rxjsService = inject(RxjsOperatorsService); 
-  private http = inject(HttpClient); 
+  private httpService = inject(HttpClient); 
   private destroy$ = new Subject<void>();
   private baseURL = 'http://localhost:5292';
 
   ngOnInit(): void {
-    // this.loadAllEmployeesAndOrders();
-    this.rxjsService.demoConcatMap();
     //this.rxjsService.demoConcatMap();
-    //this.loadEmployeeById(1);
+    this.loadEmployeeById(1);
   }
 
   loadEmployeeById(id: number) {
-    const employee$ = this.http.get<employee>(`${this.baseURL}/api/employees/${id}`);
+    const employee$ = this.httpService.get<employee>(`${this.baseURL}/api/employees/${id}`);
     employee$.subscribe({
-        next: (empdata: employee) => {
-          console.log(empdata);
-        },
-        error: (err) => {
-          console.error(err);
-        }
-      });
+      next: (employeeData) => {
+        console.log(employeeData);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
   }
 
   /**
    * example of forkjoin
    */
   loadAllEmployeesAndOrders() {
-    const orders$ = this.http.get<order[]>(
+    const orders$ = this.httpService.get<order[]>(
       `${this.baseURL}/api/orders`
     );
-
-    const employees$ = this.http.get<employee[]>(
+    const employees$ = this.httpService.get<employee[]>(
       `${this.baseURL}/api/employees`
     );
 
